0f068d1cdaf066afd44d50c36052c5f2310f985e
strelok <strelok@ecwid.com>
2021-09-03 18:47:26 +0400

    token_listener

--- background.js
+++ background.js
@@ -1,10 +1,34 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.title === "Новый документ - Google Документы") {
+    /* chrome.windows.create({
+      focused: true,
+      width: 400,
+      height: 600,
+      type: 'popup',
+      url: 'popup.html',
+      top: 0,
+      left: 0
+    },
+    () => {}) */    
+    /* chrome.runtime.sendMessage({
+      msg: "something_completed", 
+      data: {
+          subject: "Loading",
+          content: "Just completed!"
+      }
+    }); */
+
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: () => {
-        confirm("Загрузить шаблон?");
+      func: (tabId,changeInfo,tab) => {        
+        console.log(window.location.toString());
+        chrome.storage.sync.set({token: window.location.toString()}, function() {
+          console.log('Value is set to ' + window.location.toString());
+        });        
       },
+      args:[tabId,changeInfo,tab],
     });
+    
   }
 });
+
--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-console.log("попап")
+
+let btn=document.getElementById('snow');
+
+chrome.storage.sync.get(['token'], function(result) {
+    console.log('Value currently is ' + result.token);
+  });
